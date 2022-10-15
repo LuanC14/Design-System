2c9206c9c5104e7736cc461eae4dce475c6aac80
LuanC14 <luanchr14@gmail.com>
2022-10-15 14:51:13 -0300

    Change: Removed Storybook Static from gitIgnore

---
+++ storybook-static/assets/preview.5839bbf1.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.795fdcb1.js";import"./iframe.b34ce74f.js";import"./es.map.constructor.7363fc69.js";import"./es.number.to-fixed.2f3a1300.js";import"./index.eeba78da.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.5839bbf1.js.map
